765c93e1b83e78042b4de5ed727ba853f595db2d
Petr Šebek <petrsebek1@gmail.com>
2013-11-28 17:46:21 +0100

    Added test of all problems

--- main.cpp
+++ main.cpp
@@ -4,6 +4,36 @@
 #include "MathVector.hpp"
 #include "Parameters.hpp"
 
+void test(Parameters parameters)
+{
+	int success = 0;
+	int runs = 0;
+	int problem_success = 0;
+	int problem_run = 0;
+	Result r;
+	for(auto it=Tests.begin(); it!=Tests.end(); it++)
+	{
+		problem_run = 0;
+		problem_success = 0;
+		for(int dimension = 2; dimension <= 10; dimension++)
+		{
+			DimensionLimits dims(dimension, -2.048, 2.048);
+			PSO pso(it->second, dims, parameters);
+			r = pso.fmin();
+			if(r.success)
+				problem_success += 1;
+			problem_run += 1;
+		}
+		std::cout << it->first << " succeed: " << problem_success
+				 << "/" << problem_run <<  " = " 
+				 << (double) problem_success / problem_run << std::endl;
+		success += problem_success;
+		runs += problem_run;
+	}
+	std::cout << "Total succeed: " << success
+			 << "/" << runs <<  " = " 
+			 << (double) success / runs << std::endl;
+}
 
 int main(int argc, char * argv[])
 {
@@ -16,5 +46,9 @@ int main(int argc, char * argv[])
 		Result r = pso.fmin();
 		std::cout << it->first << ":" << std::endl
 				  << r;
+	}
+	else if(parameters.problem == "test")
+	{
+		test(parameters);
 	}
 }
