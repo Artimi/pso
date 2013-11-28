c458ffc2bdcd364007a76eefa146da4184b2e785
Petr Šebek <petrsebek1@gmail.com>
2013-11-28 15:38:19 +0100

    added tests

--- Testbed.cpp
+++ Testbed.cpp
@@ -1,4 +1,7 @@
+#include <map>
+#include <cmath>
 #include "Testbed.hpp"
+#define _USE_MATH_DEFINES
 
 double sphereFunction(MathVector x)
 {
@@ -7,3 +10,28 @@ double sphereFunction(MathVector x)
 		result += elem * elem;
 	return result;
 }
+
+double rosenbrockFunction(MathVector x)
+{
+	double result = 0.0;
+	for(int i = 0; i < x.size() - 1; i++)
+	{
+		result += 100 * pow(x.vector[i+1] - x.vector[i], 2.0) + pow(1 - x.vector[i], 2.0);
+	}
+	return result;
+}
+
+double rastriginFunction(MathVector x)
+{
+	double result = 10.0 * x.size();
+	for(auto elem: x.vector)
+	{
+		result += elem * elem  - 10 * std::cos(2 * M_PI * elem);
+	}
+	return result;
+}
+
+typedef double (*Testfunction)(MathVector);
+std::map<std::string, Testfunction> Tests{{"sphere", &sphereFunction},
+										 {"rosenbrock", &rosenbrockFunction},
+										 {"rastrigin", &rastriginFunction}};
